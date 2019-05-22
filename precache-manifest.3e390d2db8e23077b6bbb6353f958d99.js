48a053c2fd50ba6ac77a427e31358ef0766ef55a
Shafira Ananda <afishafiraa@gmail.com>
2019-05-22 11:13:07 +0700

    Updates

---
+++ precache-manifest.3e390d2db8e23077b6bbb6353f958d99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c894778a4c8a0e9357a86fedda366795",
+    "url": "/TA-WEB-3/index.html"
+  },
+  {
+    "revision": "ddd20aa35b650742273b",
+    "url": "/TA-WEB-3/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "774d5e3722e17f42d013",
+    "url": "/TA-WEB-3/static/js/2.cba05053.chunk.js"
+  },
+  {
+    "revision": "ddd20aa35b650742273b",
+    "url": "/TA-WEB-3/static/js/main.e49195aa.chunk.js"
+  },
+  {
+    "revision": "612d5fe054c417b5ed0a",
+    "url": "/TA-WEB-3/static/js/runtime~main.282a4eec.js"
+  }
+]);
